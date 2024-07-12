d7c05c03cb98d458d90ca9387c3f10b02749b33d
yarin1997 <yarin.brooks@gmail.com>
2024-07-12 17:41:35 +0300

    Add emojis

---
+++ game-hub/src/components/Emoji.tsx
@@ -0,0 +1,18 @@
+import  Bullseye  from "../assets/bulls-eye.webp"
+import ThumbsUp  from "../assets/thumbs-up.webp"
+import meh from "../assets/meh.webp"
+import { Image, ImageProps } from "@chakra-ui/react"
+interface Props {
+    rating: number
+}
+const Emoji = ({rating}:Props) => {
+    const emojiMap:{[key:number]:ImageProps} = {
+      3: { src: meh, alt: "meh", boxSize: "30px" },
+      4: { src: ThumbsUp, alt: "recommended", boxSize: "30px" },
+      5: { src: Bullseye, alt: "expectional", boxSize: "400px" },
+    };
+    if(rating < 3) return null
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+}
+
+export default Emoji
