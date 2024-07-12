3d2f30299c753a1391ce0f2422a8a9b8b3a24182
yarin1997 <yarin.brooks@gmail.com>
2024-07-12 17:51:56 +0300

    Customize the theme to get darker

--- game-hub/src/components/Emoji.tsx
+++ game-hub/src/components/Emoji.tsx
@@ -9,7 +9,7 @@ const Emoji = ({rating}:Props) => {
     const emojiMap:{[key:number]:ImageProps} = {
       3: { src: meh, alt: "meh", boxSize: "30px" },
       4: { src: ThumbsUp, alt: "recommended", boxSize: "30px" },
-      5: { src: Bullseye, alt: "expectional", boxSize: "400px" },
+      5: { src: Bullseye, alt: "expectional", boxSize: "40px" },
     };
     if(rating < 3) return null
   return <Image {...emojiMap[rating]} marginTop={1} />;
